c814b643937f04274fce1a4ab2e869a13fb95045
Sajiv <jsgnanasekaran@loyola.edu>
2022-08-16 23:36:05 -0400

    spinning imposter

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="/imposter.png" className="App-imposter" alt="imposter-img"  />
+        {/*
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+ 
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+         */}
       </header>
     </div>
   );
