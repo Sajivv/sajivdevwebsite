001f0e67e506afc222b1862dca2d5b69cd1779fa
Sajiv <jsgnanasekaran@loyola.edu>
2022-08-22 18:18:43 -0400

    fixed console errors from manifest

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*}
         <Helmet>
           <meta charSet="utf-8" />
           <title>Software by Sajiv</title>
           <link rel="canonical" href="http://sajiv.dev" />
           <meta name="description" content="Explore the latest software and projects designed by Sajiv" />
         </Helmet>
-  */}
         <img src={process.env.PUBLIC_URL + "/images/imposter.png"} className="App-imposter" alt="imposter-img" />
         {/*
         <img src={logo} className="App-logo" alt="logo" />
